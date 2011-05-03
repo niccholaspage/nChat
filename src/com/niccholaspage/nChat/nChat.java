7ca4f713ebbed5a2128a4066a9a628fa9225a05e
Nicholas niccholaspage Nassar <niccholaspage@gmail.com>
2011-05-03 17:27:58 -0400

    Color character comment fail

--- src/com/niccholaspage/nChat/nChat.java
+++ src/com/niccholaspage/nChat/nChat.java
@@ -19,6 +19,7 @@ public class nChat extends JavaPlugin {
     public PermissionHandler Permissions;
     //Message Format
     public String messageFormat;
+    //Color character
     public String colorCharacter;
     @Override
 	public void onDisable() {
