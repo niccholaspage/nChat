a327bc6934cdfb687e28017927279176890fa9d3
Nicholas Nassar <niccholaspage@gmail.com>
2011-07-17 02:09:51 -0400

    You can now use every option in message format in every other message. Also changed the Permissions 3 chat a bit. This is 0.5.

--- src/com/niccholaspage/nChat/nChatPlayerListener.java
+++ src/com/niccholaspage/nChat/nChatPlayerListener.java
@@ -4,82 +4,72 @@ import java.util.Date;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerListener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class nChatPlayerListener extends PlayerListener{
-	 public nChat plugin;
-	 public nChatPlayerListener(nChat plugin) {
-		 this.plugin = plugin;
-	 }
-	 public void onPlayerJoin(PlayerJoinEvent event){
-		 if (plugin.joinMessage == "") return;
-		 Player player = event.getPlayer();
-		 String[] old = new String[]{"+name", "+rname", "&"};
-		 String[] replacements = new String[]{player.getDisplayName(), player.getName(), "¤"};
-		 String out = plugin.replaceSplit(plugin.joinMessage, old, replacements);
-		 out = out.replaceAll("%", "%%");
-		 event.setJoinMessage(out);
-	 }
-	 public void onPlayerQuit(PlayerQuitEvent event){
-		 if (plugin.leaveMessage == "") return;
-		 Player player = event.getPlayer();
-		 String[] old = new String[]{"+name", "+rname", "&"};
-		 String[] replacements = new String[]{player.getDisplayName(), player.getName(), "¤"};
-		 String out = plugin.replaceSplit(plugin.leaveMessage, old, replacements);
-		 out = out.replaceAll("%", "%%");
-		 event.setQuitMessage(out);
-	 }
-	 public void onPlayerKick(PlayerKickEvent event){
-		 if (plugin.leaveMessage == "") return;
-		 Player player = event.getPlayer();
-		 String[] old = new String[]{"+name", "+rname", "&"};
-		 String[] replacements = new String[]{player.getDisplayName(), player.getName(), "¤"};
-		 String out = plugin.replaceSplit(plugin.leaveMessage, old, replacements);
-		 out = out.replaceAll("%", "%%");
-		 event.setLeaveMessage(out);
-	 }
+	public nChat plugin;
+
+	public nChatPlayerListener(nChat plugin) {
+		this.plugin = plugin;
+	}
 	@SuppressWarnings("deprecation")
-	public void onPlayerChat(PlayerChatEvent event) {
-		  Player player = event.getPlayer();
-		  String message = event.getMessage();
-		  if (message.startsWith("/")) return;
-		  String out = plugin.messageFormat;
-		  String world = player.getWorld().getName();
-		  String group;
-		  String prefix;
-		  String suffix;
-		  String userPrefix;
-		  String userSuffix;
-		  if (plugin.permissions3){
-			  group = plugin.Permissions.getPrimaryGroup(world, player.getName());
-			  prefix = plugin.Permissions.getGroupRawPrefix(world, group);
-			  suffix = plugin.Permissions.getGroupRawSuffix(world, group);
-			  userPrefix = plugin.Permissions.getUserPrefix(world, player.getName());
-			  userSuffix = plugin.Permissions.getUserSuffix(world, player.getName());
-		  }else {
-			  group = plugin.Permissions.getGroup(world, player.getName());
-			  prefix = plugin.Permissions.getGroupPrefix(world, group);
-			  suffix = plugin.Permissions.getGroupSuffix(world, group);
-			  userPrefix = plugin.Permissions.getPermissionString(world, player.getName(), "prefix");
-			  userSuffix = plugin.Permissions.getPermissionString(world, player.getName(), "suffix");
-		  }
-		  if (userPrefix != null) prefix = userPrefix;
-		  if (userSuffix != null) suffix = userSuffix;
-		  if (prefix == null) prefix = "";
-		  if (suffix == null) suffix = "";
-		  Date now = new Date();
-		  SimpleDateFormat dateFormat = new SimpleDateFormat(plugin.timestampFormat);
-		  String time = dateFormat.format(now);
-		  String[] old = new String[]{"+name", "+rname", "+group", "+prefix", "+suffix", "+world", "+timestamp", "&", "+message"};
-		  String[] replacements = new String[]{player.getDisplayName(), player.getName(), group, prefix, suffix, world, time, "¤", message};
-		  out = plugin.replaceSplit(out, old, replacements);
+	private String formatMessage(String out, PlayerEvent event){
+		if (out == null || out == ""){
+			return null;
+		}
+		String message = "";
+		if (event instanceof PlayerChatEvent){
+			message = ((PlayerChatEvent)event).getMessage();
+		}
+		Player player = event.getPlayer();
+		String world = player.getWorld().getName();
+		String group;
+		String prefix = null;
+		String suffix = null;
+		String userPrefix;
+		String userSuffix;
+		if (plugin.permissions3){
+			group = plugin.Permissions.getPrimaryGroup(world, player.getName());
+			userPrefix = plugin.Permissions.getUserPrefix(world, player.getName());
+			userSuffix = plugin.Permissions.getUserSuffix(world, player.getName());
+		}else {
+			group = plugin.Permissions.getGroup(world, player.getName());
+			prefix = plugin.Permissions.getGroupPrefix(world, group);
+			suffix = plugin.Permissions.getGroupSuffix(world, group);
+			userPrefix = plugin.Permissions.getPermissionString(world, player.getName(), "prefix");
+			userSuffix = plugin.Permissions.getPermissionString(world, player.getName(), "suffix");
+		}
+		if (userPrefix != null) prefix = userPrefix;
+		if (userSuffix != null) suffix = userSuffix;
+		if (prefix == null) prefix = "";
+		if (suffix == null) suffix = "";
+		Date now = new Date();
+		SimpleDateFormat dateFormat = new SimpleDateFormat(plugin.timestampFormat);
+		String time = dateFormat.format(now);
+		String[] old = new String[]{"+name", "+rname", "+group", "+prefix", "+suffix", "+world", "+timestamp", "&", "+message"};
+		String[] replacements = new String[]{player.getDisplayName(), player.getName(), group, prefix, suffix, world, time, "¤", message};
+		out = plugin.replaceSplit(out, old, replacements);
 		if ((plugin.Permissions.has(player, "nChat.colors")) || (plugin.Permissions.has(player, "nChat.colours"))) {
 			out = out.replace(plugin.colorCharacter, "¤");
 		}
 		out = out.replaceAll("%", "%%");
-		event.setFormat(out);
-	  }
+		return out;
+	}
+	public void onPlayerJoin(PlayerJoinEvent event){
+		event.setJoinMessage(formatMessage(plugin.joinMessage, event));
+	}
+	public void onPlayerQuit(PlayerQuitEvent event){
+		event.setQuitMessage(formatMessage(plugin.leaveMessage, event));
+	}
+	public void onPlayerKick(PlayerKickEvent event){
+		event.setLeaveMessage(formatMessage(plugin.leaveMessage, event));
+	}
+	public void onPlayerChat(PlayerChatEvent event) {
+		if (event.getMessage().startsWith("/")) return;
+		event.setFormat(formatMessage(plugin.messageFormat, event));
+	}
 }
