23e7feb63b5a620b88820421e55249dc94f82865
Nicholas Nassar <niccholaspage@gmail.com>
2012-03-31 14:31:15 -0400

    Actual revert

--- src/main/java/com/niccholaspage/nChat/permissions/PlayerPermissionsHandler.java
+++
@@ -1,9 +0,0 @@
-package com.niccholaspage.nChat.permissions;
-
-public interface PlayerPermissionsHandler {
-	public String getGroup();
-	
-	public String getPrefix();
-	
-	public String getSuffix();
-}
